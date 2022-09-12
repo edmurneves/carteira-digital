73b035c3feee26743ef000e1d7b48a71c6ee4279
Edmur <edmurneves@gmail.com>
2022-09-11 22:42:39 -0300

    Feat: Instalação da lib react-switch

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,4 +1,6 @@
 import React, { useMemo} from 'react';
+
+import Toggle from '../Toggle';
 import { emojis } from '../../utils/emojis'
 import { Container, Profile, Welcome, UserName } from './styles';
 
@@ -12,7 +14,7 @@ const MainHeader: React.FC = () => {
 
     return (
         <Container>
-            <h1>Toogle</h1>
+            <Toggle />
             <Profile>
                 <Welcome>Olá, {emoji} </Welcome>
                 <UserName>Edmur Parizoto</UserName>
