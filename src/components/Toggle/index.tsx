3069e4884c5dbcad187e516c08a9db432b2fcb6d
Edmur <edmurneves@gmail.com>
2022-09-12 00:15:48 -0300

    Feat: Função para o toggle

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 
-
+import { useState } from 'react';
 import { Container, ToggleLabel, ToggleSelector } from './styles';
 
-const Toggle: React.FC = () => (   
+const Toggle: React.FC = () => {
+
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = () => {
+        setChecked(!checked);
+    }
+ 
+    return (
     <Container>
         <ToggleLabel>Light</ToggleLabel>
         <ToggleSelector
-            checked
+            checked={checked}
             uncheckedIcon={false}
             checkedIcon={false}
-            onChange={() => console.log('mudou')}
+            onChange={handleChange}
          />
         <ToggleLabel>Dark</ToggleLabel>
     </Container>
-)
-
+    )
+} 
 export default Toggle;
