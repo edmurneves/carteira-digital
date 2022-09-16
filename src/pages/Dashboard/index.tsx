bac93fac0637c81f3bd123926e2fb865c49ca0bc
Edmur <edmurneves@gmail.com>
2022-09-16 10:16:45 -0300

    Feat: Criado a const com os valores do select

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
 import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
 
 import { Container } from './styled';
 
 const Dashboard: React.FC = () => {
+
+    const options = [
+        {value: 'Edmur', label: 'Edmur'},
+        {value: 'Regiane', label: 'Regiane'},
+        {value: 'Hanry', label: 'Hanry'},
+    ]
+
     return (        
         <Container>
-            <ContentHeader />
+            <ContentHeader title="Dashboard" lineColor="#F7931B">
+                <SelectInput options={options}/>
+            </ContentHeader>
         </Container>
     );
 }
