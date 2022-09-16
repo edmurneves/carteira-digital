e640290ec7ac3f78069e35e5db82d418b65ae216
Edmur <edmurneves@gmail.com>
2022-09-16 10:18:37 -0300

    Feat: Importado a pagina List.

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import GlobalStyles from './styles/GlobalStyles';
 import dark from './styles/themes/dark';
 
 import Layout from './components/Layout';
-import Dashboard from './pages/Dashboard';
+// import Dashboard from './pages/Dashboard';
+import List from './pages/List';
 
 const App: React.FC = () => {
     return (
         <ThemeProvider theme={dark}>
             <GlobalStyles />            
                 <Layout>
-                    <Dashboard />
+                    <List/>
                 </Layout>
         </ThemeProvider>
     );
