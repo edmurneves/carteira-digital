f6f834961c2a6130fa979b5ce3e29b2528a12b8b
Edmur <edmurneves@gmail.com>
2022-09-16 10:17:34 -0300

    Feat: Adicionado o Content header a pagina List

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
+import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
+
+import { Container } from './styles';
+
 const List: React.FC = () => {
+    const options = [
+        {value: 'Edmur', label: 'Edmur'},
+        {value: 'Regiane', label: 'Regiane'},
+        {value: 'Hanry', label: 'Hanry'},
+    ];
+
     return (
-        <h1>List</h1>
+        <Container>
+            <ContentHeader title="Saídas" lineColor="#E44C4E">
+                <SelectInput options={options}/>
+            </ContentHeader>
+
+        </Container>
     );
 }
 
