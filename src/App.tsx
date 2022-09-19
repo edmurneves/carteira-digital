2682e4697ff759c87087d356e0d498822a996308
Edmur <edmurneves@gmail.com>
2022-09-19 10:28:34 -0300

    Feat: Criadas as rotas de navegação.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
+
+import Routes from './routes';
+
 import dark from './styles/themes/dark';
 
-import Layout from './components/Layout';
-// import Dashboard from './pages/Dashboard';
-import List from './pages/List';
 
 const App: React.FC = () => {
     return (
         <ThemeProvider theme={dark}>
-            <GlobalStyles />            
-                <Layout>
-                    <List/>
-                </Layout>
+            <GlobalStyles /> 
+            <Routes />             
         </ThemeProvider>
     );
 }
